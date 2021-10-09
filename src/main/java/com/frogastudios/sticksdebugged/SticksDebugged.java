bfdca67aaf7d2585b34992de23cc02c59b89a44c
ExoPlant <lennyj2468+github@gmail.com>
2021-10-09 13:24:02 +0100

    Initial Commit

---
+++ src/main/java/com/frogastudios/sticksdebugged/SticksDebugged.java
@@ -0,0 +1,10 @@
+package com.frogastudios.sticksdebugged;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SticksDebugged implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
